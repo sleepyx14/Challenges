8ca0a9e8c16ed6f62900b8afea03ae9f2fc8fa79
Xavier Fant <fxfant@gmail.com>
2018-03-02 07:54:06 -0500

    Added ajax support for beefy lipsum api calls. Returned data appears in textarea's upon success

--- FrontEnd/SaveState_raw/javscript/SaveState_raw.js
+++ FrontEnd/SaveState_raw/javscript/SaveState_raw.js
@@ -1,16 +1,41 @@
 var curState = {};
+var beefyLipsumURL = 'https://baconipsum.com/api/?type=meat-and-filler';
 
 document.getElementById("fld1").addEventListener('change', function(){updatePageState("fld1");});
 document.getElementById("fld2").addEventListener('change', function(){updatePageState("fld2");});
 document.getElementById("fld3").addEventListener('change', function(){updatePageState("fld3");});
+
 document.getElementById("sel1").addEventListener('change', function(){updatePageState("sel1");});
 document.getElementById("sel2").addEventListener('change', function(){updatePageState("sel2");});
 document.getElementById("sel3").addEventListener('change', function(){updatePageState("sel3");});
+
 document.getElementById("btn1").addEventListener('change', function(){updatePageState("btn1");});
 document.getElementById("btn2").addEventListener('change', function(){updatePageState("btn2");});
 document.getElementById("btn3").addEventListener('change', function(){updatePageState("btn3");});
+
 document.getElementById("save").addEventListener('click', function(){savePageState();});
 document.getElementById("reset").addEventListener('click', function(){resetPageState();});
+
+document.getElementById("GETbl1").addEventListener('click', function(){
+																getAjax(beefyLipsumURL,
+																function(data){
+																	handleBeefyAjaxData(data,'fld1');
+																});
+															});
+document.getElementById("GETbl2").addEventListener('click', function(){
+																getAjax(beefyLipsumURL,
+																function(data){
+																	handleBeefyAjaxData(data,'fld2');
+																});
+															});
+document.getElementById("GETbl3").addEventListener('click', function(){
+																getAjax(beefyLipsumURL,
+																function(data){
+																	handleBeefyAjaxData(data,'fld3');
+																});
+															});
+
+
 
 function updatePageState(elmId){
 	var val = document.getElementById(elmId).value;
@@ -30,7 +55,8 @@ function updatePageState(elmId){
 	}
 
 	document.getElementById("curPageState").innerHTML = JSON.stringify(curState);
-	console.log(elmId+":"+curState[elmId]);
+	// console.log(elmId+":"+curState[elmId]);
+	console.log(curState);
 }
 
 function savePageState(){
@@ -40,10 +66,10 @@ function savePageState(){
 	try {
 		var successful = document.execCommand('copy');
 		var msg = successful ? 'successful' : 'unsuccessful';
-		console.log('Copying text command was ' + msg);
+		/* console.log('Copying text command was ' + msg); */
 	}
 	catch (err) {
-		console.log('Oops, unable to copy:'+ err);
+		/* console.log('Oops, unable to copy:'+ err); */
 	}
 }
 
@@ -52,7 +78,7 @@ function resetPageState(){
 	var jVal = JSON.parse(val);
 
 	for(key in jVal){
-		console.log("key:"+key+" ,value:"+jVal[key]);
+		/* console.log("key:"+key+" ,value:"+jVal[key]); */
 			if(key == 'btn1'){
 				document.getElementById('btn1').checked=true;
 				document.getElementById('btn2').checked=false;
@@ -71,4 +97,29 @@ function resetPageState(){
 
 			document.getElementById(key).value = jVal[key];
 	}
+}
+
+function getAjax(url, success) {
+	var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+	xhr.open('GET', url);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState>3 && xhr.status==200){
+			success(xhr.responseText);
+		}
+	};
+
+	xhr.send();
+	return xhr;
+}
+
+function handleBeefyAjaxData(data,elmId){
+	var jData = JSON.parse(data);
+	/* console.log(data); */
+	var beefyLipsum = '';
+
+	for(key in jData){
+		beefyLipsum += jData[key]+'\n';
+	}
+	document.getElementById(elmId).value = beefyLipsum;
+	updatePageState(elmId);
 }
